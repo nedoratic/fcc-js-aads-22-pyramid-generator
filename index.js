228909ea937a8a8b8e55eace4e0ca82be63ca216
nedoratic <nedoratic@gmail.com>
2024-05-14 00:41:49 +0200

    index.js - variable initialization

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// JavaScript is the programming language that powers the web. Unlike the HTML and CSS you have learned previously, JavaScript is most commonly used to write logic instead of markup.
+// Your character variable currently does not have a value. You can assign a value using the assignment operator =. For example:
 
-// One of the most important concepts in programming is variables. A variable points to a specific memory address that stores a value. Variables are given a name which can be used throughout your code to access that value.
+// let hello = "Hello";
 
-// Declaring a variable means giving it a name. In JavaScript, this is often done with the let keyword. For example, here is how you would declare a hello variable:
+// Assigning a value to a variable at the moment of its declaration is known as initialization.
 
-// let hello;
+// Initialize your character variable by assigning it the value "Hello" during its declaration.
 
-// Variable naming follows specific rules: names can include letters, numbers, dollar signs, and underscores, but cannot contain spaces and must not begin with a number.
-
-// Declare a character variable in your code.
-
-// Note: It is common practice to end statements in JavaScript with a semicolon. ;
-
-let character;
+let character = 'Hello';
