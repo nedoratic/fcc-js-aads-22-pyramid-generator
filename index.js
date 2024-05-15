9a7aa3aca69380e19cfff2908a490973810013f6
nedoratic <nedoratic@gmail.com>
2024-05-15 04:08:29 +0200

    index.js - array mutability

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// You can access the values inside an array using the index of the value. An index is a number representing the position of the value in the array, starting from 0 for the first value.
+// Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
 
-// You can access the value using bracket notation, such as array[0].
+// For example, this code would assign the number 25 to the second element in the array:
 
-// Use console.log and bracket notation to print the first value in your rows array.
+// let array = [1, 2, 3];
+
+// array[1] = 25;
+
+// console.log(array); // prints [1, 25, 3]
+
+// Update the third element of your rows array to be the number 10. Then print the rows array to your console.
 
 let character = 'Hello';
 
@@ -11,3 +17,7 @@ let count = 8;
 let rows = ['Naomi', 'Quincy', 'CamperChan'];
 
 console.log(rows[0]);
+
+rows[2] = 10;
+
+console.log(rows);
