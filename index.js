8526ee55538b2edb0245b24a51c54bc3caa6d590
nedoratic <nedoratic@gmail.com>
2024-05-15 03:47:35 +0200

    index.js - mathematical operations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Use let to declare a count variable. Assign it the number 8. When using a number value, you do not use quotes. For example:
-
-// let money = 100;
+// With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
 
 let character = 'Hello';
 
 let count = 8;
+
+console.log(count + 1);
