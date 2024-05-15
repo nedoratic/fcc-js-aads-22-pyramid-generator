047f75e373f5a8c17e5d82b31b18e3ab7b45b644
nedoratic <nedoratic@gmail.com>
2024-05-15 03:49:45 +0200

    index.js - array

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
+// In programming, you will often need to work with lots of data. There are many data structures that can help you organize and manage your data. One of the most basic data structures is an array.
+
+// An array is a non-primitive data type that can hold a series of values. Non-primitive data types differ from primitive data types in that they can hold more complex data. Primitive data types like strings and numbers can only hold one value at a time.
+
+// Arrays are denoted using square brackets ([]). Here is an example of a variable with the value of an empty array:
+
+// let array = [];
+
+// Declare a rows variable and assign it an empty array.
 
 let character = 'Hello';
 
 let count = 8;
+
+let rows = [];
 
 console.log(count + 1);
