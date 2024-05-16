d2a7da9cddbbde0343ba10bdd3ab1d3d7ff072bb
nedoratic <nedoratic@gmail.com>
2024-05-16 17:15:03 +0200

    index.js - for loop

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// Declaring a variable with the const keyword allows it to be reassigned. This means you could change character later to be a completely different value.
+// To generate a pyramid, you will need to create multiple rows. When you have to perform a task repeatedly until a condition is met, you will use a loop. There are many ways to write a loop.
 
-// For this project, you will not want to change these variable values. So instead, you should use const to declare them. const variables are special.
+// You are going to start with a basic for loop. for loops use the following syntax:
 
-// First, a const variable cannot be reassigned like a let variable. This code would throw an error:
+// for (iterator; condition; iteration) {
+//     logic;
+// }
 
-// const firstName = "Naomi";
-
-// firstName = "Jessica";
-
-// A const variable also cannot be uninitialized. This code would throw an error:
-
-// const firstName;
-
-// Replace your let keywords with const.
+// In the upcoming steps, you'll explore each component of a loop in detail. For now, construct a for loop that includes the terms "iterator", "condition", and "iteration" for the three components. Keep the loop body, the section within the curly braces {}, empty.
 
 const character = 'Hello';
 
 const count = 8;
 
 const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+	rows.push(i);
+}
+
+console.log(rows);
