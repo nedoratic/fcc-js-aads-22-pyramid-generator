b3bd5be88d120f1728c55073529df827a06c66ce
nedoratic <nedoratic@gmail.com>
2024-05-17 01:56:39 +0200

    index.js - padRow function

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const count = 8;
 
 const rows = [];
 
-// You're getting closer! At this point, you're encountering what's known as an off-by-one error, a frequent problem in zero-based indexing languages like JavaScript.
-
-// The first index of your rows array is 0, which is why you start your for loop with i = 0. But repeating a string zero times results in nothing to print.
-
-// To fix this, add 1 to the value of i in your .repeat() call. Do not assign it back to i like you did in your loop conditions.
-
 for (let i = 0; i < count; i = i + 1) {
 	rows.push(character.repeat(i + 1));
 }
@@ -21,3 +15,20 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+// The logic for formatting this pyramid is likely going to get complicated, which means it's a great time to extract that code into a function.
+
+// A function is a block of code that can be reused throughout your application. Functions are declared with the following syntax:
+
+// function name(parameter) {
+
+// }
+// The function keyword tells JavaScript that the name variable is going to be a function. parameter is a variable that represents a value that is passed into the function when it is used. A function may have as many, or as few, parameters as you'd like. Like a for loop, the space between the curly braces is the function body.
+
+// Declare a padRow function. Do not create any parameter variables yet. The function body should be empty. Remember that you need to use camel case for your naming convention.
+
+function padRow() {}
+
+const call = padRow();
+
+console.log(call);
