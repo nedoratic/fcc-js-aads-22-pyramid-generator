b3d2194f00dec96f1233fd5f895c5371df7484d6
nedoratic <nedoratic@gmail.com>
2024-05-16 17:24:35 +0200

    index.js - for of loop

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ for (let i = 0; i < count; i = i + 1) {
 }
 
 console.log(rows);
+
+let result = '';
+
+console.log(result);
+
+// To manipulate the result string, you will use a different type of loop. Specifically, a for...of loop, which iterates over each item in an iterable object and temporarily assigns it to a variable.
+
+// The syntax for a for...of loop looks like:
+
+// for (const value of iterable) {
+
+// }
+
+// Note that you can use const because the variable only exists for a single iteration, not during the entire loop.
+
+// Create a for...of loop to iterate through your rows array, assigning each value to a row variable.
+
+for (const row of rows) {
+}
