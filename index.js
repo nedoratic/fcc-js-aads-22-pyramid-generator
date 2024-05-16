5ac30785df1c4e2cef5a11c881af448695aeacc9
nedoratic <nedoratic@gmail.com>
2024-05-17 01:48:57 +0200

    index.js - repeat method

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const character = 'Hello';
+const character = '#';
 
 const count = 8;
 
 const rows = [];
 
+// Now you have a series of # characters, but the pyramid shape is still missing. Fortunately, the i variable represents the current "row" number in your loop, enabling you to use it for crafting a pyramid-like structure.
+
+// To achieve this, you will use the .repeat() method available to strings. This method accepts a number as an argument, specifying the number of times to repeat the target string. For example, using .repeat() to generate the string "Code! Code! Code!":
+
+// const activity = "Code! ";
+
+// activity.repeat(3);
+
+// Use the .repeat() method on your character, and give it i for the number.
+
 for (let i = 0; i < count; i = i + 1) {
-	rows.push(i);
+	rows.push(character.repeat(i));
 }
 
 let result = '';
-
-// Now all of your numbers are appearing on the same line. This will not work for creating a pyramid.
-
-// You will need to add a new line to each row. However, pressing the return key to insert a line break between quotes in JavaScript will result in a parsing error. Instead, you need to use the special escape sequence \n, which is interpreted as a new line when the string is logged. For example:
-
-// lineOne = lineOne + "\n" + lineTwo;
-
-// Use a second addition operator to concatenate a new line between the existing result value and the added row value.
 
 for (const row of rows) {
 	result = result + '\n' + row;
