8df5f976c98ac2fee3d76bc98a574ca6df1a5e25
nedoratic <nedoratic@gmail.com>
2024-05-18 21:41:56 +0200

    index.js - local scope variable

--- index.js
+++ index.js
@@ -16,38 +16,15 @@ for (const row of rows) {
 
 console.log(result);
 
-// When you have a value that is explicitly written in your code, like the "Hello!" string in your function, it is considered to be hard-coded. Hard-coding a value inside a function might not make it as reusable as you'd like.
+// Variables can also be declared inside a function. These variables are considered to be in the local scope, or block scope. A variable declared inside a function can only be used inside that function. If you try to access it outside of the function, you will either get undefined or an error.
 
-// Instead, you can define parameters for the function. Parameters are special variables that are given a value when you call the function, and can be used in your function to dynamically change the result of the function's code.
+// To see this in action, use const to declare a test variable in your padRow function. Initialise it with the value "Testing".
 
-// To add a parameter to your function, you need to add a variable name inside the parentheses. For example, this demo function has a name parameter:
-
-// function demo(name) {
-
-// }
-
-// name sounds like a useful parameter, so go ahead and add it to your padRow function.
-
-// A function does not have to return a hard-coded value. It can return the value stored in a variable. Parameters are special variables for a function, so they can also be returned.
-
-// Change your padRow function to return the name parameter directly.
-
-// Variables in JavaScript are available in a specific scope. In other words, where a variable is declared determines where in your code it can be used.
-
-// The first scope is the global scope. Variables that are declared outside of any "block" like a function or for loop are in the global scope. Your character, count, and rows variables are all in the global scope.
-
-// When a variable is in the global scope, a function can access it in its definition. Here is an example of a function using a global title variable:
-
-// const title = "Professor ";
-// function demo(name) {
-//   return title + name;
-// }
-// demo("Naomi")
-
-// This example would return "Professor Naomi". Update your padRow function to return the value of concatenating your character variable to the beginning of the name parameter.
+// Then, below your function, try to log test to the console. You will see an error because it is not defined outside of the function's local scope. Remove that console.log to pass the tests and continue.
 
 function padRow(name) {
 	return character + name;
+	const test = 'Testing';
 }
 
 const call = padRow('CamperChan');
