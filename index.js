fe4833fb881981eec2c939b644a8db4215612f8d
nedoratic <nedoratic@gmail.com>
2024-05-18 21:47:24 +0200

    index.js - return keyword position

--- index.js
+++ index.js
@@ -16,15 +16,13 @@ for (const row of rows) {
 
 console.log(result);
 
-// Variables can also be declared inside a function. These variables are considered to be in the local scope, or block scope. A variable declared inside a function can only be used inside that function. If you try to access it outside of the function, you will either get undefined or an error.
+// There is a bit of a problem, here. Your test declaration is currently after your return statement. An important thing to know about the return keyword is that it does not just define a value to be returned from your function, it also stops the execution of your function code. This means that any code after a return statement will not run.
 
-// To see this in action, use const to declare a test variable in your padRow function. Initialise it with the value "Testing".
-
-// Then, below your function, try to log test to the console. You will see an error because it is not defined outside of the function's local scope. Remove that console.log to pass the tests and continue.
+// Move your test initialization to the line above your return statement.
 
 function padRow(name) {
-	return character + name;
 	const test = 'Testing';
+	return character + name;
 }
 
 const call = padRow('CamperChan');
