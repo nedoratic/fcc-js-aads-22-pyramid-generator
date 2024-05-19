d4f2e9f3ba836ef105bdde328d2c856a0866397b
nedoratic <nedoratic@gmail.com>
2024-05-19 20:09:31 +0200

    index.js - calling a function inside method

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const character = '#';
-
 const count = 8;
-
 const rows = [];
 
+function padRow(rowNumber, rowCount) {
+	return character.repeat(rowNumber);
+}
+
+// A function call allows you to actually use a function. You may not have been aware of it, but the methods like .push() that you have been using have been function calls.
+
+// A function is called by referencing the function's name, and adding (). Here's how to call a test function:
+
+// test();
+
+// Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
+
 for (let i = 0; i < count; i = i + 1) {
-	rows.push(character.repeat(i + 1));
+	rows.push(padRow());
 }
 
 let result = '';
@@ -15,21 +25,3 @@ for (const row of rows) {
 }
 
 console.log(result);
-
-// Remember in an earlier step, you learned about return values. A function can return a value for your application to consume separately.
-
-// In a function, the return keyword is used to specify a return value. For example, this function would return the value given to the first parameter:
-
-// function name(parameter) {
-//   return parameter;
-// }
-
-// Use the return keyword to return the value of the character variable, repeated rowNumber times.
-
-function padRow(rowNumber, rowCount) {
-	return character.repeat(rowNumber);
-}
-
-const call = padRow();
-
-console.log(call);
