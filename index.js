84ebfbfdf8e5a534931d9d8553e51834c2a04098
nedoratic <nedoratic@gmail.com>
2024-05-19 20:11:29 +0200

    index.js - function arguments

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ function padRow(rowNumber, rowCount) {
 	return character.repeat(rowNumber);
 }
 
-// A function call allows you to actually use a function. You may not have been aware of it, but the methods like .push() that you have been using have been function calls.
+// Your padRow function has two parameters which you defined. Values are provided to those parameters when a function is called.
 
-// A function is called by referencing the function's name, and adding (). Here's how to call a test function:
+// The values you provide to a function call are referred to as arguments, and you pass arguments to a function call. Here's a function call with "Hello" passed as an argument:
 
-// test();
+// test("Hello");
 
-// Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
+// Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
 
 for (let i = 0; i < count; i = i + 1) {
-	rows.push(padRow());
+	rows.push(padRow(i + 1, count));
 }
 
 let result = '';
