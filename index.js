9f10ee5dea6cc1a9413e08798b542c60a5e3e518
nedoratic <nedoratic@gmail.com>
2024-05-20 21:46:03 +0200

    index.js - function edited

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const character = '#';
 const count = 8;
 const rows = [];
 
-// Now it is time for a bit of math. Consider a three-row pyramid. If we want it centered, it would look something like:
+// You can pass full expressions as an argument. The function will receive the result of evaluating that expression. For example, these two function calls would yield the same result:
+
+// test(2 * 3 + 1);
+// test(7);
+// Looking at the pattern again:
 
 // ··#··
 // ·###·
 // #####
 
-// Empty spaces have been replaced with interpuncts, or middle dots, for readability. If you extrapolate the pattern, you can see that the spaces at the beginning and end of a row follow a pattern.
+// Update the character value to be repeated 2 * rowNumber - 1 times.
 
-// Update your blank space strings to be repeated rowCount - rowNumber times.
-
-// Open up the console to see the result.
+// Open up the console again to see the updated result.
 
 function padRow(rowNumber, rowCount) {
-	return ' '.repeat(rowCount - rowNumber) + character.repeat(rowNumber) + ' '.repeat(rowCount - rowNumber);
+	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
 for (let i = 0; i < count; i = i + 1) {
