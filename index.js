1d2093adbb553c765b6f9d60b93e61cf7e81482a
nedoratic <nedoratic@gmail.com>
2024-05-20 21:49:06 +0200

    index.js - addition assignment operator

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ const character = '#';
 const count = 8;
 const rows = [];
 
-// You can pass full expressions as an argument. The function will receive the result of evaluating that expression. For example, these two function calls would yield the same result:
-
-// test(2 * 3 + 1);
-// test(7);
-// Looking at the pattern again:
-
-// ··#··
-// ·###·
-// #####
-
-// Update the character value to be repeated 2 * rowNumber - 1 times.
-
-// Open up the console again to see the updated result.
-
 function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
+// Your pyramid generator now functions as expected. But this is an excellent opportunity to further explore the code you have written.
+
+// The addition operator is not the only way to add values to a variable. The addition assignment operator can be used as shorthand to mean "take the original value of the variable, add this value, and assign the result back to the variable." For example, these two statements would yield the same result:
+
+// test = test + 1;
+
+// test += 1;
+
+// Update your iterator statement in the for loop to use addition assignment.
+
+for (let i = 0; i < count; i += 1) {
 	rows.push(padRow(i + 1, count));
 }
 
