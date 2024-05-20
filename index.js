638e07fcd35c7c024f51013863e6a336a4d97645
nedoratic <nedoratic@gmail.com>
2024-05-20 22:03:42 +0200

    index.js - if statement

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
-// JavaScript also has support for multi-line comments. A multi-line comment starts with /* and ends with */.
-
-// Unlike a single-line comment, a multi-line comment will encapsulate multiple lines.
-
-// Use /* and */ to turn your current for loop, including the body, into a multi-line comment.
-
-// Change the loop type from a for loop to a while loop.
+// TO DO: Change the loop type from a for loop to a while loop.
 /*
     for (let i = 1; i <= count; i++) {
 	    rows.push(padRow(i, count));
