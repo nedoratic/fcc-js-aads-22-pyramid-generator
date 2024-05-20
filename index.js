7407c211c907a87ac942200acc40232d2348c163
nedoratic <nedoratic@gmail.com>
2024-05-20 21:58:16 +0200

    index.js - single line comments

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
-// Unfortunately, now the bottom of the pyramid has disappeared. This is because you have created another off-by-one error.
+// Comments can be helpful for explaining why your code takes a certain approach, or leaving to-do notes for your future self.
 
-// Your original loop went for i values from 0 to 7, because count is 8 and your condition requires i to be less than count. Your loop is now running for i values from 1 to 7.
+// In JavaScript, you can use // to leave a single-line comment in your code.
 
-// Your loop needs to be updated to run when i is 8, too. Looking at your logic, this means your loop should run when i is less than or equal to count. You can use the less than or equal to operator <= for this.
+// Add a single-line comment above your function to remind yourself to change the code to a different kind of loop.
 
-// Update your loop condition to run while i is less than or equal to count.
-
+// Change the loop type from a for loop to a while loop.
 for (let i = 1; i <= count; i++) {
 	rows.push(padRow(i, count));
 }
