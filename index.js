fca8a2250687d683cacfeb8a5d0978a40de1fea4
nedoratic <nedoratic@gmail.com>
2024-05-21 21:03:41 +0200

    index.js - infinite loop

--- index.js
+++ index.js
@@ -14,31 +14,17 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
-// The text has appeared again! This is because "false" is a string, which when evaluated to a boolean becomes true. This means "false" is a truthy value.
-
-// A truthy value is a value that is considered true when evaluated as a boolean. Most of the values you encounter in JavaScript will be truthy.
-
-// A falsy value is the opposite - a value considered false when evaluated as a boolean. JavaScript has a defined list of falsy values. Some of them include false, 0, "", null, undefined, and NaN.
-
-// Try changing your if condition to an empty string "", which is a falsy value.
-
-// The text is gone again! Empty strings evaluate to false, making them a falsy value. You will learn more about truthy and falsy values in future projects.
-
-// For now, remove your if statement entirely. Use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.
-
 let continueLoop = false;
 
 let done = 0;
 
-// A while loop will run over and over again until the condition specified is no longer true. It has the following syntax:
+// Right now, if you change continueLoop to true, your while loop will run forever. This is called an infinite loop, and you should be careful to avoid these. An infinite loop can lock up your system, requiring a full restart to escape.
 
-// while (condition) {
-//    logic;
-// }
+// To avoid this, start by using the increment operator to increase the value of the done variable inside your loop.
 
-// Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.
-
-while (continueLoop) {}
+while (continueLoop) {
+	done++;
+}
 
 let result = '';
 
