5006d3bfe4cf24d00d9a5b6f83579ad260d0ed52
nedoratic <nedoratic@gmail.com>
2024-05-21 20:58:26 +0200

    index.js - truthy and falsy values

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
-// TO DO: Change the loop type from a for loop to a while loop.
+// TODO: use a different type of loop
+
 /*
     for (let i = 1; i <= count; i++) {
-	    rows.push(padRow(i, count));
+        rows.push(padRow(i, count));
     }
 */
+
+// The text has appeared again! This is because "false" is a string, which when evaluated to a boolean becomes true. This means "false" is a truthy value.
+
+// A truthy value is a value that is considered true when evaluated as a boolean. Most of the values you encounter in JavaScript will be truthy.
+
+// A falsy value is the opposite - a value considered false when evaluated as a boolean. JavaScript has a defined list of falsy values. Some of them include false, 0, "", null, undefined, and NaN.
+
+// Try changing your if condition to an empty string "", which is a falsy value.
+
+if ('false') {
+	console.log('Condition is true');
+}
 
 let result = '';
 
