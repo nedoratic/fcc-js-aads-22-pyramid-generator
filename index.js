a014097f578dfde54428e7d52624d6aea399c90a
nedoratic <nedoratic@gmail.com>
2024-05-21 21:06:24 +0200

    index.js - equality operator

--- index.js
+++ index.js
@@ -22,8 +22,15 @@ let done = 0;
 
 // To avoid this, start by using the increment operator to increase the value of the done variable inside your loop.
 
+// The equality operator == is used to check if two values are equal. To compare two values, you'd use a statement like value == 8.
+
+// Add an if statement to your loop. The statement should check if done is equal to count using the equality operator.
+
 while (continueLoop) {
 	done++;
+
+	if (done === count) {
+	}
 }
 
 let result = '';
