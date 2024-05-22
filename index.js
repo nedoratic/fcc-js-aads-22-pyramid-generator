cac204e7462f764cf7b04b47f1854d8a894be933
nedoratic <nedoratic@gmail.com>
2024-05-22 21:48:44 +0200

    index.js - if statement removed

--- index.js
+++ index.js
@@ -18,18 +18,12 @@ let continueLoop = false;
 
 let done = 0;
 
-// The strict inequality operator !== allows you to check if two values are not equal, or do not have the same type. The syntax is similar to the equality operator: value !== 4.
-
-// Update your while loop condition to check if done is not equal to count.
+// Since you have moved the comparison into the while condition, you can remove your entire if statement.
 
 while (done !== count) {
 	done++;
 
 	rows.push(padRow(done, count));
-
-	if (done === count) {
-		continueLoop = false;
-	}
 }
 
 let result = '';
