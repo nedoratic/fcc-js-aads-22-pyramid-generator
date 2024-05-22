04203e76617518779f965bfa2fe911ae9e49cf7f
nedoratic <nedoratic@gmail.com>
2024-05-22 21:47:25 +0200

    index.js - strict inequality operator

--- index.js
+++ index.js
@@ -18,19 +18,11 @@ let continueLoop = false;
 
 let done = 0;
 
-// The equality operator can lead to some strange behavior in JavaScript. For example, "0" == 0 is true, even though one is a string and one is a number.
+// The strict inequality operator !== allows you to check if two values are not equal, or do not have the same type. The syntax is similar to the equality operator: value !== 4.
 
-// The strict equality operator === is used to check if two values are equal and share the same type. As a general rule, this is the equality operator you should always use. With the strict equality operator, "0" === 0 becomes false, because while they might have the same value of zero, they are not of the same type.
+// Update your while loop condition to check if done is not equal to count.
 
-// Update your done == count condition to use the strict equality operator.
-
-// When done has reached the value of count, we want the loop to stop executing.
-
-// Inside your if body, assign the boolean false to your continueLoop variable.
-
-// To make your pyramid generate again, push the result of calling padRow with done and count as the arguments to your rows array, similar to what you did in your first loop.
-
-while (continueLoop) {
+while (done !== count) {
 	done++;
 
 	rows.push(padRow(done, count));
