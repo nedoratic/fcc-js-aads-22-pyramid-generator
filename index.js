c323085f800746e7507cc6a2fdb14e380afe6f0c
nedoratic <nedoratic@gmail.com>
2024-05-22 21:59:03 +0200

    index.js - done variable removed

--- index.js
+++ index.js
@@ -14,18 +14,9 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
-let done = 0;
-
-// You can also replace the done reference in your padRow call.
-
-// Note that rows.length here would give you an off-by-one error, because done is incremented before the call.
-
-// So you'll need to replace done here with rows.length + 1. When you do this, you may see a Range Error, because we've created another off-by-one error.
-
-// You'll need to change the while condition to use the less than operator, instead of the less than or equal operator.
+// Now you no longer need your done variable. Remove the increment operation from your loop, and the variable declaration for done.
 
 while (rows.length < count) {
-	done++;
 	rows.push(padRow(rows.length + 1, count));
 }
 
