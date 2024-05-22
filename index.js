531d027f945aab2e134d429c8e97d974693ebea2
nedoratic <nedoratic@gmail.com>
2024-05-22 21:51:47 +0200

    index.js - continue loop variable deleted

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
-let continueLoop = false;
+// Your loop is no longer relying on the continueLoop variable. This makes the variable an unused declaration. Generally, you want to avoid unused declarations to prevent future confusion.
+
+// Remove your continueLoop variable.
 
 let done = 0;
-
-// Since you have moved the comparison into the while condition, you can remove your entire if statement.
 
 while (done !== count) {
 	done++;
