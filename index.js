6c8bd4f9f3cd2cebe0cea805d17863a41439470a
nedoratic <nedoratic@gmail.com>
2024-05-22 21:54:58 +0200

    index.js - lenght property

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
-// Your loop is no longer relying on the continueLoop variable. This makes the variable an unused declaration. Generally, you want to avoid unused declarations to prevent future confusion.
-
-// Remove your continueLoop variable.
-
 let done = 0;
 
-while (done !== count) {
+// Your pyramid generator is still working. However, it could be possible to end up with an infinite loop again.
+
+// Because you are only checking if done is not equal to count, if done were to be larger than count your loop would go on forever.
+
+// Update your loop's condition to check if done is less than or equal to count.
+
+// Using done to track the number of rows that have been generated is functional, but you can actually clean up the logic a bit further.
+
+// Arrays have a special length property that allows you to see how many values, or elements, are in the array. You would access this property using syntax like myArray.length.
+
+// Update your condition to check if rows.length is less than or equal to count.
+
+while (rows.length <= count) {
 	done++;
 
 	rows.push(padRow(done, count));
