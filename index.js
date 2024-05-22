776593171672e77e8e5e1cc9c41d9ae3bb59aa0a
nedoratic <nedoratic@gmail.com>
2024-05-22 22:04:10 +0200

    index.js - for loop body added

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ function padRow(rowNumber, rowCount) {
 
 // TODO: Invert pyramid
 
+// What if you made your pyramid upside-down, or inverted? Time to try it out!
+
+// Start by creating a new for loop. Declare your iterator i and assign it the value of count, then use the boolean false for your condition and iteration statements.
+
+for (let i = count; false; false) {}
+
 let result = '';
 
 for (const row of rows) {
