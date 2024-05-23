bd31876d18695a4afca5e2935aae54c8f5be1f56
nedoratic <nedoratic@gmail.com>
2024-05-23 20:49:12 +0200

    index.js - for loop commented out

--- index.js
+++ index.js
@@ -18,15 +18,11 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
-// TODO: Invert pyramid
-
-// Because you are only subtracting one from i, you can use the decrement operator --.
-
-// Replace your subtraction assignment with the decrement operator.
-
-for (let i = count; i > 0; i--) {
-	rows.push(padRow(i, count));
-}
+/*
+    for (let i = count; i > 0; i--) {
+        rows.push(padRow(i, count));
+    }
+*/
 
 let result = '';
 
