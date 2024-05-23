d26a8b7833e44416e05fc8d01132608ede2e9908
nedoratic <nedoratic@gmail.com>
2024-05-23 21:05:34 +0200

    index.js - unshift method

--- index.js
+++ index.js
@@ -24,11 +24,17 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
-// You can actually build the inverted pyramid without needing to loop "backwards" like you did.
+// The .unshift() method of an array allows you to add a value to the beginning of the array, unlike .push() which adds the value at the end of the array. Here is an example:
 
-// To do this, you'll need to learn a couple of new array methods. Start by using const to declare a numbers variable. Assign it an array with the elements 1, 2, and 3. Then log the numbers array.
+// const numbers = [1, 2, 3];
+// numbers.unshift(5);
+// The numbers array would now be [5, 1, 2, 3].
+
+// Use const to declare an unshifted variable, and assign it the result of calling .unshift() on your numbers array. Pass 5 as the argument. Then print your unshifted variable.
 
 const numbers = [1, 2, 3];
+
+const unshifted = numbers.unshift(5);
 
 console.log(numbers);
 
