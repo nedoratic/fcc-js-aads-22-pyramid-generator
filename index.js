3a2dda05d1c56bf43913a6a5ba34088fcd4ebb02
nedoratic <nedoratic@gmail.com>
2024-05-23 20:52:48 +0200

    index.js - numbers array added

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ function padRow(rowNumber, rowCount) {
     }
 */
 
+// You can actually build the inverted pyramid without needing to loop "backwards" like you did.
+
+// To do this, you'll need to learn a couple of new array methods. Start by using const to declare a numbers variable. Assign it an array with the elements 1, 2, and 3. Then log the numbers array.
+
+const numbers = [1, 2, 3];
+
+console.log(numbers);
+
 let result = '';
 
 for (const row of rows) {
