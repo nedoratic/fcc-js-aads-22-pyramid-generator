b4b7dfc090b659218a14536448e3e16cf1034e18
nedoratic <nedoratic@gmail.com>
2024-05-24 21:44:16 +0200

    index.js - unshift method

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
-// Sometimes you may wish to bring back previous code that you commented out. You can do so by removing the /* and */ around that code. This is called uncommenting.
+// Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
 
-// Uncomment only your first for loop. Leave the single line comment and the other two multi line comments in place.
+// Update your loop body to add new rows to the beginning of the array.
 
 for (let i = 1; i <= count; i++) {
-	rows.push(padRow(i, count));
+	rows.unshift(padRow(i, count));
 }
 
 /*
