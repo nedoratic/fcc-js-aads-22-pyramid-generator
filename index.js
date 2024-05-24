6ed51d7fff542910cf592e777509879282b9c011
nedoratic <nedoratic@gmail.com>
2024-05-24 22:05:17 +0200

    index.js - comments removed

--- index.js
+++ index.js
@@ -8,19 +8,9 @@ function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
 
-// Sometimes you will want to run different code when an if condition is false. You can do this by adding an else block. An else block will only evaluate if the if block does not evaluate.
+// Your pyramid generator is now in a finished state, with more functionality than you originally planned! The next step is to clean up your code.
 
-// The syntax to add an else block is:
-
-// if (condition) {
-//   logic
-// } else {
-//   logic
-// }
-
-// Add an else block to your if block.
-
-// When inverted is false, you want to build a standard pyramid. Use .push() like you have in previous steps to achieve this.
+// Remove all comments, both single- and multi-line, from your code.
 
 for (let i = 1; i <= count; i++) {
 	if (inverted) {
@@ -29,18 +19,6 @@ for (let i = 1; i <= count; i++) {
 		rows.push(padRow(i, count));
 	}
 }
-
-/*
-    while (rows.length < count) {
-        rows.push(padRow(rows.length + 1, count));
-    }
-*/
-
-/*
-    for (let i = count; i > 0; i--) {
-        rows.push(padRow(i, count));
-    }
-*/
 
 let result = '';
 
