62ed65c0d847d019a4091f3217e12e03e838a36a
nedoratic <nedoratic@gmail.com>
2024-05-24 21:51:33 +0200

    index.js - boolean variable

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const character = '#';
 const count = 8;
 const rows = [];
 
+// What if you had a way to toggle between an inverted pyramid and a standard pyramid?
+
+// Start by declaring an inverted variable, and assigning it the value true. You are not changing this variable in your code, but you will need to use let so our tests can modify it later.
+
+let inverted = true;
+
 function padRow(rowNumber, rowCount) {
 	return ' '.repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + ' '.repeat(rowCount - rowNumber);
 }
-
-// Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
-
-// Update your loop body to add new rows to the beginning of the array.
 
 for (let i = 1; i <= count; i++) {
 	rows.unshift(padRow(i, count));
